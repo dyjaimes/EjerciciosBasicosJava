0ccf8874a85af86a2d9af084942f990a5d779324
Dauri Yidarli Jaimes Dominici <136355772+dyjaimes@users.noreply.github.com>
2025-02-18 21:00:50 -0500

    avance
    Avance de la actividad POO

--- Principal/src/principal/Ejercicio1.java
+++ Principal/src/principal/Ejercicio1.java
@@ -4,15 +4,25 @@ import java.util.Scanner;
 public class Ejercicio1 {
     
     
-    private int piso=0;
+    private int piso;
     
     public void tomarDatos(Scanner obtener){
-        System.out.println("escriba el numero de piso al que se dirije, (1 al 10): ");
         int planta;
-       planta=obtener.nextInt();
-       if(planta>=1 && planta<=10){
-       piso=planta;
-       }
+        System.out.println("escriba el numero de piso al que se dirije, (1/10): ");
+
+        do{
+                  planta=obtener.nextInt();
+       
+       
+                  if(planta>=1 && planta<=10){
+                           piso=planta;}
+                  else{
+                           System.out.println("el numero es incorrecto, vuelva a digitar el numero (1/10)");
+                        }
+      
+        }while(planta<=0 || planta>10);
+    
+       
     }
     public int mostrarDatos(){
         
--- Principal/src/principal/Principal.java
+++ Principal/src/principal/Principal.java
@@ -1,5 +1,6 @@
 package principal;
 import java.util.Scanner;
+import java.util.Set;
 
 
 
@@ -9,9 +10,10 @@ public class Principal {
     public static void main(String[] args) {
       
         Scanner obtener =new Scanner(System.in);
-        System.out.println("escriba el numero del ejercico a realizar: ");
-        int numero;
-        
+        System.out.println("Ejercicios de programacion basica con metodologia Poo");
+        int numero, peso, piso, temperatura;
+        String mensaje;
+               
         
         do {
          System.out.println("Ejercicio 1");
@@ -24,18 +26,71 @@ public class Principal {
          System.out.println("Ejercicio 8");
          System.out.println("Ejercicio 9");
          System.out.println("Ejercicio 10");
+         System.out.println("escriba el numero del ejercico a realizar:  ");
          numero=obtener.nextInt();
         }while(numero<0 || numero>10);
          
         switch (numero){
+            
             case 1:
                 Ejercicio1 miEjercicio1 =new Ejercicio1();
                 miEjercicio1.tomarDatos(obtener);
-                System.out.println("csdkcksdcsd"+miEjercicio1.mostrarDatos());
+                System.out.println("Desplazandose al piso #: "+miEjercicio1.mostrarDatos());
+                obtener.close();
                 break;
+                
             case 2:
                 
+                        System.out.println("ingrese la edad: ");
+                        do{
+                            piso=obtener.nextInt();    
+                            if(piso<0)
+                            System.out.println("vuelva a ingresar la edad: ");
+
+                        }while(piso<0);
+                   
+                         System.out.println("ingrese su peso: ");
+                         do{
+                             peso=obtener.nextInt();
+                             if(peso<0)
+                             System.out.println("vuelva a ingresar su peso: ");
+                         }while(peso<0);
+                         
+                Ejercicio2 miEjercicio2=new Ejercicio2(peso,piso, obtener);
+                System.out.println("Peso permitido, peso total "+miEjercicio2.getPesoPermitido()+"kilogramos");
+                break;
                 
+            case 3:
+                
+                Ejercicio3 miEjercicio3=new Ejercicio3();
+                temperatura=miEjercicio3.obtenerNumero(obtener, "ingrese la temperatura actual: ");
+                System.out.println("el numero ingresado es "+temperatura);
+                
+                if(temperatura>=18&& temperatura<=25)
+                    System.out.println("la temperatura es adecuada");
+                
+                else if(temperatura>25)
+                    System.out.println("el ambiente es caloroso");
+                
+                else
+                    System.out.println("el ambiente es frio");
+                
+                miEjercicio3.setTemperatura(temperatura);
+                System.out.println("temperatura de "+  miEjercicio3.getRegistroTemperatura()+" grados registrada en el campo ");
+                break;
+                
+            case 4:
+                
+                System.out.print(" ingrese la temperatura ambiente: ");
+                temperatura=obtener.nextInt();
+                Ejercicio4 miEjercicio4=new Ejercicio4(obtener, temperatura);
+                
+              
+                if(miEjercicio4.setTemperatura()>8&&miEjercicio4.setTemperatura()<34)
+                    System.out.println("la temperatura es ideal con  "+  miEjercicio4.setTemperatura()+" grados ");
+                
+                else
+                  System.out.println("la temperatura es de  "+  miEjercicio4.setTemperatura()+" grados ");
         }
         
     }
